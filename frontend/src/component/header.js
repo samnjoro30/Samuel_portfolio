75e7c1370fb8e62f8b823ccc268af39cba2d8eee
Sam njoro <samnjorokibandi@gmail.com>
2025-07-20 12:42:47 +0300

    updates

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { FaGithub, FaLinkedin, FaTwitter, FaDownload, FaPaperPlane } from 'react-icons/fa';
-import profileImg from '../assets/sam1.jpeg';
+import profileImg from '../assets/sam5.jpeg';
 import CV from '../assets/Samuel_Njoroge_Kibandi_Resume_Update_6_2025.pdf';
 import '../styles/header.css';
 

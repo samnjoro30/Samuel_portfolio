7a78ddc9fcf29b5d614e0aed9ff9058f769660f2
Sam njoro <samnjorokibandi@gmail.com>
2025-10-27 20:12:57 +0300

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Header from './component/header';
